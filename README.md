6b786c1b643c4ba4456166b4126b465fd21fbbf0
Paul Miller <paul@dgtlrepublic.com>
2017-03-22 01:42:14 -0400

    add eprime detection

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 [![NPM version][npm-image]][npm-url]
 [![Build status][travis-image]][travis-url]
 [![Test coverage][coveralls-image]][coveralls-url]
+[![Downloads][downloads-image]][downloads-url]
 
 # e-prime
 
@@ -26,3 +27,5 @@ MIT
 [travis-url]: https://travis-ci.org/Vorror/e-prime
 [coveralls-image]: https://img.shields.io/coveralls/Vorror/e-prime.svg?style=flat-square
 [coveralls-url]: https://coveralls.io/r/Vorror/e-prime
+[downloads-image]: http://img.shields.io/npm/dm/coalesce-strategy.svg?style=flat-square
+[downloads-url]: https://npmjs.org/package/coalesce-strategy
---
+++ test/tests.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var chai = require('chai');
+var mocha = require('mocha');
+var assert = chai.assert;
+var eprime = require('../e-prime');
+
+describe('e-prime', function () {
+    it('Should detect non-e\' word', function (done) {
+        assert.deepEqual(eprime('NodeJs is awesome ;)'), [{
+            index: 7,
+            offset: 2
+        }]);
+
+        done();
+    });
+
+    it('Should detect \'was\'', function (done) {
+        assert.deepEqual(eprime('So the cat was stolen'), [{
+            index: 11,
+            offset: 3
+        }]);
+
+        done();
+    });
+
+    it('Should detect \'I\'m\'', function (done) {
+        assert.deepEqual(eprime('I\'m not sure where to delivery the pizza'), [{
+            index: 0,
+            offset: 3
+        }]);
+
+        done();
+    });
+
+    it('Shouldn\'t detect anything', function (done) {
+        assert.deepEqual(eprime('Welcome to the jungle.'), []);
+        done();
+    });
+
+    it('Should detect multiple suggestions', function (done) {
+        assert.deepEqual(eprime('I\'m not sure where to delivery the pizza because my map was stolen'), [{
+            index: 0,
+            offset: 3
+        }, {
+            index: 56,
+            offset: 3
+        }]);
+
+        done();
+    });
+});
